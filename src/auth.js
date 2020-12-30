662e33d1df942b8165d125a39dfd14c15ceef210
exodojaffar <exodojaffar@gmail.com>
2020-12-30 19:06:39 -0400

    Cria um modulo de autenticação com base no @google-cloud/local-auth

--- src/auth.js
+++ src/auth.js
@@ -4,7 +4,7 @@ const {authenticate} = require('@google-cloud/local-auth');
 
 const Authenticate = async (scopes, file_path, google) => {
 	const auth = await authenticate({
-			scopes: scopes
+			scopes: scopes, 
 			keyfilePath: file_path
 		})
 
